e56ce2cbe18875d1adb5ba0358d410d3de04a4a3
Cameron Lloyd <Cam.Lloyd728@gmail.com>
2021-03-18 23:42:57 -0500

    added all the boxes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,10 @@ namespace Assignment5
             this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -81,17 +85,57 @@ namespace Assignment5
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(334, 186);
+            this.panel1.Location = new System.Drawing.Point(367, 211);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(373, 411);
             this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(512, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Answers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 391);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Answer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1057, 391);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Running Total";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(512, 824);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Running Total";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Teal;
-            this.ClientSize = new System.Drawing.Size(1131, 782);
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(1131, 846);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -112,6 +156,10 @@ namespace Assignment5
         private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,14 +13,14 @@ namespace Assignment5
     public partial class Form1 : Form
     {
         RichTextBox[,] EasyBoxes = new RichTextBox[3, 3];
-        RichTextBox[] EasyAnswers = new RichTextBox[7];
-        RichTextBox[] EasyTotals = new RichTextBox[7];
+        RichTextBox[] EasyAnswers = new RichTextBox[8];
+        RichTextBox[] EasyTotals = new RichTextBox[8];
         RichTextBox[,] MediumBoxes = new RichTextBox[5, 5];
-        RichTextBox[] MediumAnswers = new RichTextBox[11];
-        RichTextBox[] MediumTotals = new RichTextBox[11];
+        RichTextBox[] MediumAnswers = new RichTextBox[12];
+        RichTextBox[] MediumTotals = new RichTextBox[12];
         RichTextBox[,] HardBoxes = new RichTextBox[7, 7];
-        RichTextBox[] HardAnswers = new RichTextBox[15];
-        RichTextBox[] HardTotals = new RichTextBox[15];
+        RichTextBox[] HardAnswers = new RichTextBox[16];
+        RichTextBox[] HardTotals = new RichTextBox[16];
         public Form1()
         {
             InitializeComponent();
@@ -88,6 +88,7 @@ namespace Assignment5
                 this.EasyAnswers[i].Text = i.ToString();
                 this.EasyAnswers[i].ReadOnly = true;
                 this.EasyAnswers[i].SelectionAlignment = HorizontalAlignment.Center;
+                this.EasyAnswers[i].BackColor = Color.Gray;
                 this.Controls.Add(EasyAnswers[i]);
                 EasyTotals[i] = new System.Windows.Forms.RichTextBox();
                 this.EasyTotals[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 44F);
@@ -107,10 +108,11 @@ namespace Assignment5
             this.EasyAnswers[3].Text = 3.ToString();
             this.EasyAnswers[3].ReadOnly = true;
             this.EasyAnswers[3].SelectionAlignment = HorizontalAlignment.Center;
+            this.EasyAnswers[3].BackColor = Color.Gray;
             this.Controls.Add(EasyAnswers[3]);
             EasyTotals[3] = new System.Windows.Forms.RichTextBox();
             this.EasyTotals[3].Font = new System.Drawing.Font("Microsoft Sans Serif", 44F);
-            this.EasyTotals[3].Location = new System.Drawing.Point(panel1.Location.X + panel1.Width + 15, panel1.Location.Y + panel1.Height + 15);
+            this.EasyTotals[3].Location = new System.Drawing.Point(panel1.Location.X + panel1.Width + 15, panel1.Location.Y - (panel1.Height / 3) - 15);
             this.EasyTotals[3].Name = "EasyTotal" + 3;
             this.EasyTotals[3].Size = new System.Drawing.Size(panel1.Width / 3, panel1.Height / 3);
             this.EasyTotals[3].Text = 3.ToString();
@@ -127,6 +129,7 @@ namespace Assignment5
                 this.EasyAnswers[i].Text = i.ToString();
                 this.EasyAnswers[i].ReadOnly = true;
                 this.EasyAnswers[i].SelectionAlignment = HorizontalAlignment.Center;
+                this.EasyAnswers[i].BackColor = Color.Gray;
                 this.Controls.Add(EasyAnswers[i]);
                 EasyTotals[i] = new System.Windows.Forms.RichTextBox();
                 this.EasyTotals[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 44F);
@@ -138,6 +141,25 @@ namespace Assignment5
                 this.EasyTotals[i].SelectionAlignment = HorizontalAlignment.Center;
                 this.Controls.Add(EasyTotals[i]);
             }
+            EasyAnswers[7] = new System.Windows.Forms.RichTextBox();
+            this.EasyAnswers[7].Font = new System.Drawing.Font("Microsoft Sans Serif", 44F);
+            this.EasyAnswers[7].Location = new System.Drawing.Point(panel1.Location.X - (panel1.Width / 3) - 15, panel1.Location.Y + panel1.Height + 15);
+            this.EasyAnswers[7].Name = "EasyAnswer" + 7;
+            this.EasyAnswers[7].Size = new System.Drawing.Size(panel1.Width / 3, panel1.Height / 3);
+            this.EasyAnswers[7].Text = 7.ToString();
+            this.EasyAnswers[7].ReadOnly = true;
+            this.EasyAnswers[7].SelectionAlignment = HorizontalAlignment.Center;
+            this.EasyAnswers[7].BackColor = Color.Gray;
+            this.Controls.Add(EasyAnswers[7]);
+            EasyTotals[7] = new System.Windows.Forms.RichTextBox();
+            this.EasyTotals[7].Font = new System.Drawing.Font("Microsoft Sans Serif", 44F);
+            this.EasyTotals[7].Location = new System.Drawing.Point(panel1.Location.X + panel1.Width + 15, panel1.Location.Y + panel1.Height + 15);
+            this.EasyTotals[7].Name = "EasyTotal" + 7;
+            this.EasyTotals[7].Size = new System.Drawing.Size(panel1.Width / 3, panel1.Height / 3);
+            this.EasyTotals[7].Text = 7.ToString();
+            this.EasyTotals[7].ReadOnly = true;
+            this.EasyTotals[7].SelectionAlignment = HorizontalAlignment.Center;
+            this.Controls.Add(EasyTotals[7]);
         }
 
         private void mediumToolStripMenuItem_Click(object sender, EventArgs e)
@@ -170,6 +192,7 @@ namespace Assignment5
                 this.MediumAnswers[i].Text = i.ToString();
                 this.MediumAnswers[i].ReadOnly = true;
                 this.MediumAnswers[i].SelectionAlignment = HorizontalAlignment.Center;
+                this.MediumAnswers[i].BackColor = Color.Gray;
                 this.Controls.Add(MediumAnswers[i]);
                 MediumTotals[i] = new System.Windows.Forms.RichTextBox();
                 this.MediumTotals[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
@@ -189,10 +212,11 @@ namespace Assignment5
             this.MediumAnswers[5].Text = 5.ToString();
             this.MediumAnswers[5].ReadOnly = true;
             this.MediumAnswers[5].SelectionAlignment = HorizontalAlignment.Center;
+            this.MediumAnswers[5].BackColor = Color.Gray;
             this.Controls.Add(MediumAnswers[5]);
             MediumTotals[5] = new System.Windows.Forms.RichTextBox();
             this.MediumTotals[5].Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.MediumTotals[5].Location = new System.Drawing.Point(panel1.Location.X + panel1.Width + 15, panel1.Location.Y + panel1.Height + 15);
+            this.MediumTotals[5].Location = new System.Drawing.Point(panel1.Location.X + panel1.Width + 15, panel1.Location.Y - (panel1.Height / 5) - 15);
             this.MediumTotals[5].Name = "MediumTotal" + 5;
             this.MediumTotals[5].Size = new System.Drawing.Size(panel1.Width / 5, panel1.Height / 5);
             this.MediumTotals[5].Text = 5.ToString();
@@ -209,6 +233,7 @@ namespace Assignment5
                 this.MediumAnswers[i].Text = i.ToString();
                 this.MediumAnswers[i].ReadOnly = true;
                 this.MediumAnswers[i].SelectionAlignment = HorizontalAlignment.Center;
+                this.MediumAnswers[i].BackColor = Color.Gray;
                 this.Controls.Add(MediumAnswers[i]);
                 MediumTotals[i] = new System.Windows.Forms.RichTextBox();
                 this.MediumTotals[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
@@ -220,6 +245,25 @@ namespace Assignment5
                 this.MediumTotals[i].SelectionAlignment = HorizontalAlignment.Center;
                 this.Controls.Add(MediumTotals[i]);
             }
+            MediumAnswers[11] = new System.Windows.Forms.RichTextBox();
+            this.MediumAnswers[11].Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.MediumAnswers[11].Location = new System.Drawing.Point(panel1.Location.X - (panel1.Width / 5) - 15, panel1.Location.Y + panel1.Height + 15);
+            this.MediumAnswers[11].Name = "MediumAnswer" + 11;
+            this.MediumAnswers[11].Size = new System.Drawing.Size(panel1.Width / 5, panel1.Height / 5);
+            this.MediumAnswers[11].Text = 11.ToString();
+            this.MediumAnswers[11].ReadOnly = true;
+            this.MediumAnswers[11].SelectionAlignment = HorizontalAlignment.Center;
+            this.MediumAnswers[11].BackColor = Color.Gray;
+            this.Controls.Add(MediumAnswers[11]);
+            MediumTotals[11] = new System.Windows.Forms.RichTextBox();
+            this.MediumTotals[11].Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.MediumTotals[11].Location = new System.Drawing.Point(panel1.Location.X + panel1.Width + 15, panel1.Location.Y + panel1.Height + 15);
+            this.MediumTotals[11].Name = "MediumTotal" + 11;
+            this.MediumTotals[11].Size = new System.Drawing.Size(panel1.Width / 5, panel1.Height / 5);
+            this.MediumTotals[11].Text = 11.ToString();
+            this.MediumTotals[11].ReadOnly = true;
+            this.MediumTotals[11].SelectionAlignment = HorizontalAlignment.Center;
+            this.Controls.Add(MediumTotals[11]);
         }
 
         private void hardToolStripMenuItem_Click(object sender, EventArgs e)
@@ -252,6 +296,7 @@ namespace Assignment5
                 this.HardAnswers[i].Text = i.ToString();
                 this.HardAnswers[i].ReadOnly = true;
                 this.HardAnswers[i].SelectionAlignment = HorizontalAlignment.Center;
+                this.HardAnswers[i].BackColor = Color.Gray;
                 this.Controls.Add(HardAnswers[i]);
                 HardTotals[i] = new System.Windows.Forms.RichTextBox();
                 this.HardTotals[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
@@ -271,10 +316,11 @@ namespace Assignment5
             this.HardAnswers[7].Text = 7.ToString();
             this.HardAnswers[7].ReadOnly = true;
             this.HardAnswers[7].SelectionAlignment = HorizontalAlignment.Center;
+            this.HardAnswers[7].BackColor = Color.Gray;
             this.Controls.Add(HardAnswers[7]);
             HardTotals[7] = new System.Windows.Forms.RichTextBox();
             this.HardTotals[7].Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.HardTotals[7].Location = new System.Drawing.Point(panel1.Location.X + panel1.Width + 15, panel1.Location.Y + panel1.Height + 15);
+            this.HardTotals[7].Location = new System.Drawing.Point(panel1.Location.X + panel1.Width + 15, panel1.Location.Y - (panel1.Height / 7) - 15);
             this.HardTotals[7].Name = "EasyTotal" + 7;
             this.HardTotals[7].Size = new System.Drawing.Size(panel1.Width /7, panel1.Height / 7);
             this.HardTotals[7].Text = 7.ToString();
@@ -291,6 +337,7 @@ namespace Assignment5
                 this.HardAnswers[i].Text = i.ToString();
                 this.HardAnswers[i].ReadOnly = true;
                 this.HardAnswers[i].SelectionAlignment = HorizontalAlignment.Center;
+                this.HardAnswers[i].BackColor = Color.Gray;
                 this.Controls.Add(HardAnswers[i]);
                 HardTotals[i] = new System.Windows.Forms.RichTextBox();
                 this.HardTotals[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
@@ -302,6 +349,25 @@ namespace Assignment5
                 this.HardTotals[i].SelectionAlignment = HorizontalAlignment.Center;
                 this.Controls.Add(HardTotals[i]);
             }
+            HardAnswers[15] = new System.Windows.Forms.RichTextBox();
+            this.HardAnswers[15].Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.HardAnswers[15].Location = new System.Drawing.Point(panel1.Location.X - (panel1.Width / 7) - 15, panel1.Location.Y + panel1.Height + 15);
+            this.HardAnswers[15].Name = "EasyAnswer" + 15;
+            this.HardAnswers[15].Size = new System.Drawing.Size(panel1.Width / 7, panel1.Height / 7);
+            this.HardAnswers[15].Text = 15.ToString();
+            this.HardAnswers[15].ReadOnly = true;
+            this.HardAnswers[15].SelectionAlignment = HorizontalAlignment.Center;
+            this.HardAnswers[15].BackColor = Color.Gray;
+            this.Controls.Add(HardAnswers[15]);
+            HardTotals[15] = new System.Windows.Forms.RichTextBox();
+            this.HardTotals[15].Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.HardTotals[15].Location = new System.Drawing.Point(panel1.Location.X + panel1.Width + 15, panel1.Location.Y + panel1.Height + 15);
+            this.HardTotals[15].Name = "EasyTotal" + 15;
+            this.HardTotals[15].Size = new System.Drawing.Size(panel1.Width / 7, panel1.Height / 7);
+            this.HardTotals[15].Text = 15.ToString();
+            this.HardTotals[15].ReadOnly = true;
+            this.HardTotals[15].SelectionAlignment = HorizontalAlignment.Center;
+            this.Controls.Add(HardTotals[15]);
         }
 
     }
